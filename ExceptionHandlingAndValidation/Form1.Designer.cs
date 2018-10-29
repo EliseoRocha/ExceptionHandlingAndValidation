8f5c78a26fbff65d2ea0ce583bf79818018d433d
Eliseo Rocha <rochaeliseo91@gmail.com>
2018-10-29 15:59:18 -0700

    Initial commit

---
+++ ExceptionHandlingAndValidation/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ExceptionHandlingAndValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.ValidateAgeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.Location = new System.Drawing.Point(182, 98);
+            this.AgeTextBox.MaxLength = 3;
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AgeTextBox.TabIndex = 0;
+            // 
+            // ValidateAgeButton
+            // 
+            this.ValidateAgeButton.Location = new System.Drawing.Point(182, 158);
+            this.ValidateAgeButton.Name = "ValidateAgeButton";
+            this.ValidateAgeButton.Size = new System.Drawing.Size(100, 31);
+            this.ValidateAgeButton.TabIndex = 1;
+            this.ValidateAgeButton.Text = "Validate Age";
+            this.ValidateAgeButton.UseVisualStyleBackColor = true;
+            this.ValidateAgeButton.Click += new System.EventHandler(this.ValidateAgeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 316);
+            this.Controls.Add(this.ValidateAgeButton);
+            this.Controls.Add(this.AgeTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.Button ValidateAgeButton;
+    }
+}
+
---
+++ ExceptionHandlingAndValidation/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExceptionHandlingAndValidation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ValidateAgeButton_Click(object sender, EventArgs e)
+        {
+            //Have to convert to byte in order to assign variable
+            //Having .Text is for getting the actual text from the Text
+            try
+            {
+                byte age = Convert.ToByte(AgeTextBox.Text);
+                
+                if(age > 130 || age < 13)
+                    throw new OverflowException();
+                MessageBox.Show("Thanks for the age!");
+            }
+            catch (FormatException formatException)
+            {
+                MessageBox.Show("Your age must be a number!");
+            }
+            catch(OverflowException overflowException)
+            {
+                MessageBox.Show("Age must be between 13 and 130!");
+            }
+            finally
+            {
+                //Finally block ALWAYS Executes whether an Excpetion is triggered or not
+                //ALWAYS!!!
+                //Common uses
+                //Close databas connecitons
+                //Close file connections
+            }
+
+            //catch(Exception someEx)
+            //{
+            //    MessageBox.Show("Enter a valid Number!");
+            //    MessageBox.Show(someEx.Message);
+            //    MessageBox.Show(someEx.StackTrace);
+            //    MessageBox.Show(someEx.GetType().ToString());
+            //}
+            MessageBox.Show("Test");
+        }
+    }
+}
